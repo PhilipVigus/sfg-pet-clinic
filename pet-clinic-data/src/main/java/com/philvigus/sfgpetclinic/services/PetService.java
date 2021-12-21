f6b260f42a83b6d48f30da08c5a371b4c7b50628
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-21 11:43:20 +0000

    Implement basic service interfaces
    Closes #5

---
+++ pet-clinic-data/src/main/java/com/philvigus/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.philvigus.sfgpetclinic.services;
+
+import com.philvigus.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save (Pet pet);
+
+    Set<Pet> findAll();
+}
