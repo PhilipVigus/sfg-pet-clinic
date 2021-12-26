495f1bdb439ccb8d7d0ea2e095bb7daacba68233
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-26 15:50:19 +0000

    Autogenerate map ids for models

--- pet-clinic-data/src/main/java/com/philvigus/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/philvigus/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,14 +1,11 @@
 package com.philvigus.sfgpetclinic.services.map;
 
-import com.philvigus.sfgpetclinic.services.CrudService;
+import com.philvigus.sfgpetclinic.model.BaseEntity;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -18,8 +15,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object == null) {
+            throw new RuntimeException("Object cannot be null");
+        }
+
+        if (object.getId() == null) {
+            object.setId(getNextId());
+        }
+
+        map.put(object.getId(), object);
 
         return object;
     }
@@ -30,5 +35,9 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        return map.size() > 0 ? Collections.max(map.keySet()) + 1 : 1;
     }
 }
