aee2276a1967fff4e738dc3dc7536dce91dae2f1
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-29 15:42:18 +0000

    Save vet specialities correctly

--- pet-clinic-data/src/main/java/com/philvigus/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/philvigus/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.philvigus.sfgpetclinic.services.map;
 
+import com.philvigus.sfgpetclinic.model.Speciality;
 import com.philvigus.sfgpetclinic.model.Vet;
+import com.philvigus.sfgpetclinic.services.SpecialityService;
 import com.philvigus.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +28,13 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        vet.getSpecialities().forEach(speciality -> {
+            if (speciality.getId() == null) {
+                Speciality savedSpeciality = specialityService.save((speciality));
+                speciality.setId(savedSpeciality.getId());
+            }
+        });
+
         return super.save(vet);
     }
 
