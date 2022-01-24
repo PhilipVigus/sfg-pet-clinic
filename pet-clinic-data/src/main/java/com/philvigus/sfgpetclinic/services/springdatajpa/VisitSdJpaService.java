53cc9fd6a86fca34347c8b29986c141eddb0afb7
PhilipVigus <philipvigus@yahoo.co.uk>
2022-01-24 17:07:06 +0000

    Add Visit Spring Data JPA service

---
+++ pet-clinic-data/src/main/java/com/philvigus/sfgpetclinic/services/springdatajpa/VisitSdJpaService.java
@@ -0,0 +1,48 @@
+package com.philvigus.sfgpetclinic.services.springdatajpa;
+
+import com.philvigus.sfgpetclinic.model.Visit;
+import com.philvigus.sfgpetclinic.repositories.VisitRepository;
+import com.philvigus.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSdJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSdJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
